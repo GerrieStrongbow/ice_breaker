4d1f7c867a87219310e9ad927a727466a6a07d5b
Gerhard Bekker <g.bekker@engys.com>
2025-04-29 10:10:10 +0200

    Added linkedin_lookup_agent

---
+++ agents/linkedin_lookup_agent.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.tools import Tool
+from langchain.agents import create_react_agent, AgentExecutor
+from langchain import hub
+
+load_dotenv()
+
+
+def lookup(name: str) -> str:
+    return "https://www.linkedin.com/in/gerhard-bekker-a6867b14a/"
